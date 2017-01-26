dfc2f041f5cb351ee747c5dee573dcb4fd240617
andriqueliu <andriqueliu@gmail.com>
2017-01-25 21:19:16 -0800

    Making more changes to gate

--- lab2/workingdirectory/gate.sv
+++ lab2/workingdirectory/gate.sv
@@ -2,43 +2,49 @@
 // gate is a lower level module that dictates whether a gondola may cross
 // Authors: Andrique Liu, Emraj Sidhu, Nikhil Grover
 
-//Should an output be added that tells you if the water levels are good?
-//like indicated in the rightWater module?
+/*
+gate functions as a gate in the Pound Lock system.
+
+gate's output indicates whether the gate is open or closed; an active
+gateClosed signal indicates the gate is open (gondola is free to pass),
+and a clear signal indicates the gate is closed.
+*/
 module gate(clk, reset, openCase, open);
-	
-	input logic clk, reset;
-	input logic openCase;
-	output logic open;
+	input  logic clk, reset; // Clock, reset signals
+	input  logic openSignal; // Signal to open the gate
+	output logic gateOpen;   // Is the gate open?
 	
 	//State variables
-	enum {OPEN, CLOSE}, ps, ns;
+	enum { OPEN, CLOSED } ps, ns;
 	
+	// Combinational Logic
 	always_comb begin
 		case (ps)
-		
-		OPEN:
-			if(!openCase) begin
-				ns = CLOSE;
-				open = 0;
-			end else begin
-				ns = OPEN;
-				open = 1;
-			end
-		CLOSE:
-			if(!openCase) begin
-				ns = CLOSE;
-				open = 0;
-			end else begin
-				ns = OPEN;
-				open = 1;
-			end
+			OPEN:
+				if(!openCase) begin
+					ns = CLOSE;
+					open = 0;
+				end else begin
+					ns = OPEN;
+					open = 1;
+				end
+			CLOSED:
+				if(!openCase) begin
+					ns = CLOSE;
+					open = 0;
+				end else begin
+					ns = OPEN;
+					open = 1;
+				end
 		endcase
 	end
 	
+	// Sequential Logic
 	always_ff @(posedge clk) begin
 		if(reset)
-			ps <= CLOSE; //reset as closed
+			ps <= CLOSED; // Reset to closed
 		else
 			ps <= ns;
 	end
+	
 endmodule
