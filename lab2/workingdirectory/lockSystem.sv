00dfe523c6a00b0d671d8a6bbec1b3ce5e5b53c7
andriqueliu <andriqueliu@gmail.com>
2017-01-26 02:07:22 -0800

    Writing comments. Still need to test

--- lab2/workingdirectory/lockSystem.sv
+++ lab2/workingdirectory/lockSystem.sv
@@ -25,19 +25,79 @@ module lockSystem (clk, reset, increase, decrease, gateR, gateL,
 	assign gondInL = inLeft;
 	assign gondInR = inRight;
 	assign gondInChamber = inChamber;
-
+	
+	// Hook up modules together
 	chamberWater chWater (.clk, .reset, .increase, .decrease,
 	                      .rightGood, .leftGood);
 	chamber ch (.clk, .reset, .gateR, .gateL,
 					.gondInWater(inChamber), .gondInR(inRight), .gondInL(inLeft),
 					.rightGood, .leftGood);
-	leftWater  leWater (.clk, .reset, .gateL(gateL), .waterLevelsGood(leftGood),
+					//
+	leftWater  leWater (.clk, .reset, .gateL, .waterLevelsGood(leftGood),
 	                    .gondInChamber(inChamber), .gondInWater(inLeft));
-	rightWater riWater (.clk, .reset, .gateR(gateR), .waterLevelsGood(rightGood),
+	rightWater riWater (.clk, .reset, .gateR, .waterLevelsGood(rightGood),
 	                    .gondInChamber(inChamber), .gondInWater(inRight));
 	gate leGate (.clk, .reset, .openSignal(gateL), .gateClosed(gateLClosed));
 	gate riGate (.clk, .reset, .openSignal(gateR), .gateClosed(gateRClosed));
 	
+endmodule
+
+module lockSystem_testbench();
+	logic clk, reset;         // Clock, reset
+	logic increase, decrease; // Increase/decrease water levels
+	logic gateR, gateL;       // Open/close left and right gates
+	// Output to LEDRs; indicate where gondola is
+	logic gondInL, gondInChamber, gondInR;
+	// Output to LEDRs; indicate whether gates are open or closed
+	logic gateRClosed, gateLClosed;
 	
+	lockSystem dut (clk, reset, increase, decrease, gateR, gateL,
+                   gondInL, gondInChamber, gondInR,
+						 gateRClosed, gateLClosed);
 	
+	// Set up the clock.
+	parameter CLOCK_PERIOD=100;
+	initial begin
+		clk <= 0;
+		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	end
+	
+	// Set up the inputs to the design. Each line is a clock cycle.
+	initial begin
+								  @(posedge clk);
+	reset <= 1; 			  @(posedge clk);
+								  @(posedge clk);
+	reset <= 0;            @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+	$stop; // End the simulation.
+	end
 endmodule
