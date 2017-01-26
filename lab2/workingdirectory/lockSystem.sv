46d363ce396d8702ae57ddb7201b29ca400dbe76
andriqueliu <andriqueliu@gmail.com>
2017-01-26 01:57:09 -0800

    No errors in compile message. Testing now

--- lab2/workingdirectory/lockSystem.sv
+++ lab2/workingdirectory/lockSystem.sv
@@ -1,23 +1,38 @@
 /*
 
+I/O of modules:
+
+
 */
-module lockSystem (clk, reset, );
+module lockSystem (clk, reset, increase, decrease, gateR, gateL,
+                   gondInL, gondInChamber, gondInR,
+						 gateRClosed, gateLClosed);
 	input  logic clk, reset;
 	input  logic increase, decrease;
 	input  logic gateR, gateL; // Open/close left and right gates
 	output logic gondInL, gondInChamber, gondInR;
 	output logic gateRClosed, gateLClosed;
 	
+	logic  rightGood, leftGood; // 
+	logic  inLeft, inChamber, inRight;
+	
+	assign gondInL = inLeft;
+	assign gondInR = inRight;
+	assign gondInChamber = inChamber;
+	
+	// Outputs r and leftGood to r, lWater, and chamber
 	chamberWater chWater (.clk, .reset, .increase, .decrease,
-	                      .rightGood(), .leftGood());
+	                      .rightGood, .leftGood);
 	chamber ch (.clk, .reset, .gateR, .gateL,
-					.gondInWater, .gondInR, .gondInL,
+					.gondInWater(inChamber), .gondInR(inRight), .gondInL(inLeft),
 					.rightGood, .leftGood);
-	leftWater  leWater (.clk, .reset, .gateL, .waterLevelsGood,
-	                    .gondInChamber, .gondInWater););
-	rightWater riWater (.clk, .reset, .gateR, .waterLevelsGood,
-	                    .gondInChamber, .gondInWater);
+	leftWater  leWater (.clk, .reset, .gateL(gateL), .waterLevelsGood(leftGood),
+	                    .gondInChamber(inChamber), .gondInWater(inLeft));
+	rightWater riWater (.clk, .reset, .gateR(gateR), .waterLevelsGood(rightGood),
+	                    .gondInChamber(inChamber), .gondInWater(inRight));
 	gate leGate (.clk, .reset, .openSignal(gateL), .gateClosed(gateLClosed));
 	gate riGate (.clk, .reset, .openSignal(gateR), .gateClosed(gateRClosed));
+	
+	
 	
 endmodule
