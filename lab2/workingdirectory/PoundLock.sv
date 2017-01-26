d430f2f4de1a68027a1d10163dc0c4e3a879bace
andriqueliu <andriqueliu@gmail.com>
2017-01-26 14:59:10 -0800

    Working w/o arrival delay

--- lab2/workingdirectory/PoundLock.sv
+++ lab2/workingdirectory/PoundLock.sv
@@ -48,7 +48,7 @@ module PoundLock (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	                     .enable(increaseEn), .busy(increaseBusy));
 	delayInput decDelay (.clk(clk[whichClock]), .reset, .start(decrease),
 	                     .enable(decreaseEn), .busy(decreaseBusy));
-	delayInput #(.MINUTES(5), .CLOCK(25000000)) arrDelay (.clk(clk[whichClock]), .reset, .start(arriving),
+	delayInput arrDelay (.clk(clk[whichClock]), .reset, .start(arriving),
 	                     .enable(arrivalEn), .busy(arrivalBusy));
 	
 	// Board Version
@@ -64,7 +64,7 @@ module PoundLock (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	             .arrivingOut(LEDR[0]), .departingOut(LEDR[1]),
                 .gateR, .gateL,
 					 .gondInRLEDR(LEDR[7]), .gondInLLEDR(LEDR[9]), .gondInChamberLEDR(LEDR[8]),
-					 .increaseEnable(increaseEn), .decreaseEnable(decreaseEn), .arrivingEnable(arriving),
+					 .increaseEnable(increaseEn), .decreaseEnable(decreaseEn), .arrivingEnable(1'b0),
 					 .increaseBusy(1'b0), .decreaseBusy(1'b0), .arrivingBusy(1'b0),
 					 .leftGood(LEDR[5]), .rightGood(LEDR[4]));
 	
