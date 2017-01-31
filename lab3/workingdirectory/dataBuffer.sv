39bbcd688dda2de1607fe30987787a46820baa30
andriqueliu <andriqueliu@gmail.com>
2017-01-31 12:55:21 -0800

    dataBuffer compiles successfully. Still needs testing and polishing

--- lab3/workingdirectory/dataBuffer.sv
+++ lab3/workingdirectory/dataBuffer.sv
@@ -3,18 +3,18 @@ EE 371 Lab 3 Project
 Designing an Underwater Digital Scanning System
 Authors: Andrique Liu, Nikhil Grover, Emraj Sidhu
 
-dataBuffer 
+dataBuffer controls the data level of the data buffer. 
+
 
 Note: Assume we are using 25 MHz clock (whichClock = 0)
 
 Note: Since Quartus does not allow floats, int bufferAmount goes up to 20 in
 increments of 1 instead of up to 10 in increments of 0.5.
 */
-module dataBuffer (clk, reset, );
+module dataBuffer (clk, reset, level80, level90, level100);
 	input  logic clk, reset;
-	input  logic 
 	
-	output logic 
+	output logic level80, level90, level100;
 	
 	int count;
 	int bufferAmount;
@@ -26,19 +26,36 @@ module dataBuffer (clk, reset, );
 	
 	// Combinational Logic
 	always_comb begin
-		
+		if (bufferAmount == 16) begin
+			level80 = 1;
+			level90 = 0;
+			level100 = 0;
+		end else if (bufferAmount == 18) begin
+			level80 = 0;
+			level90 = 1;
+			level100 = 0;
+		end else if (bufferAmount == 20) begin
+			level80 = 0;
+			level90 = 0;
+			level100 = 1;
+		end else begin
+			level80 = 0;
+			level90 = 0;
+			level100 = 0;
+		end
 	end
-	
 	
 	// Sequential Logic
 	always_ff @(posedge clk) begin
 		if (reset) begin
 			count <= 0;
 			bufferAmount <= 0;
-		end else if () begin
-			
+		end else if (count == (25000000 - 1)) begin // Probably want a smaller value for testing in MS
+			count <= 0;
+			bufferAmount <= bufferAmount + 1;
+		end else begin
+			count <= count + 1;
 		end
 	end
-	
 	
 endmodule
