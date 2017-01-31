1fe2ad8b49137b92abb76e16d1d1be83ba400f87
andriqueliu <andriqueliu@gmail.com>
2017-01-31 13:01:11 -0800

    Add states to scanner.sv. Still needs to be fleshed out more.

--- lab3/workingdirectory/scanner.sv
+++ lab3/workingdirectory/scanner.sv
@@ -12,48 +12,30 @@ Note: Assume we are using 25 MHz clock (whichClock = 0)
 */
 module scanner (clk, reset, );
 	input  logic clk, reset;
+	input  logic 
 	
-	output logic level80, level90, level100;
+	output logic 
 	
-	int count;
-	int bufferAmount;
 	
-	initial begin
-		count = 0;
-		bufferAmount = 0;
-	end
+	
+	
+	dataBuffer dataBuff (.clk, .reset, .level80(), .level90(), .level100());
+	
+	
+	enum { LOWPOWER, STANDBY, ACTIVE, IDLE } ps, ns;
+	
 	
 	// Combinational Logic
 	always_comb begin
-		if (bufferAmount == 16) begin
-			level80 = 1;
-			level90 = 0;
-			level100 = 0;
-		end else if (bufferAmount == 18) begin
-			level80 = 0;
-			level90 = 1;
-			level100 = 0;
-		end else if (bufferAmount == 20) begin
-			level80 = 0;
-			level90 = 0;
-			level100 = 1;
-		end else begin
-			level80 = 0;
-			level90 = 0;
-			level100 = 0;
-		end
+		
 	end
 	
 	// Sequential Logic
 	always_ff @(posedge clk) begin
 		if (reset) begin
-			count <= 0;
-			bufferAmount <= 0;
-		end else if (count == (25000000 - 1)) begin // Probably want a smaller value for testing in MS
-			count <= 0;
-			bufferAmount <= bufferAmount + 1;
+			
 		end else begin
-			count <= count + 1;
+			ps <= ns;
 		end
 	end
 	
