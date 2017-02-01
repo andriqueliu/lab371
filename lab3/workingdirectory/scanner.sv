f8cb7b28dde38fc4b472d09f978cfffd54f9fe69
Emrajs <Emrajs@uw.edu>
2017-02-01 03:40:49 -0800

    Add files via upload

--- lab3/workingdirectory/scanner.sv
+++ lab3/workingdirectory/scanner.sv
@@ -1,42 +1,62 @@
-/*
-EE 371 Lab 3 Project
-Designing an Underwater Digital Scanning System
-Authors: Andrique Liu, Nikhil Grover, Emraj Sidhu
+// EE 371 Winter 2017
+//Authors: Emraj Sidhu, Andrique Liu, Nikhil Grover 
+//Finite State Machine for the scanner
 
-scanner
-
-
-Note: Assume we are using 25 MHz clock (whichClock = 0)
+//The scanning state in the FSM may need some work. Directions for that are a bit confusing
+//We may have too many or the completely wrong inputs. We need to talk about the databuffer module and
+//how the rest of the system will be designed.
 
 
-*/
-module scanner (clk, reset, );
+module scanner (clk, reset, standBySig, startScanning, transferCmd, secondTransferCmd, transComplete, flush, stateOut );
 	input  logic clk, reset;
-	input  logic 
+	input  logic standBySign, startScannning, transferCmd, secondTransferCmd, transComplete, flush;
 	
-	output logic 
+	output logic stateOut; //is output going to hex????
 	
 	
 	
 	
 	dataBuffer dataBuff (.clk, .reset, .level80(), .level90(), .level100());
 	
+	reg [2:0] ps;
+	reg [2:0] ns;
 	
-	enum { LOWPOWER, STANDBY, ACTIVE, IDLE } ps, ns;
+	parameter lowPower = 3'b000, standby = 3'b001, scanning = 3'b010, idle = 3'b011, transfer = 3'b100,
+				 flushing = 3'b101;
+	// Present state to next state logic
+	always@(*) 
+		case(ps) 
+			lowPower : if (standBySig)        ns = standby;
+				        else                   ns = ps;
+		   standby:   if (startScanning)     ns = scanning;
+						  else                   ns = ps;
+			scanning:  if (transferCmd)       ns = transfer; 
+						  else if (!transferCmd) ns = idle;
+						  else                   ns = ps;
+			idle:      if (secondTransferCmd) ns = transfer;
+						  else if (flush)        ns = flushing;
+				        else                   ns = ps;
+			transfer:  if (transComplete)     ns = lowPower;
+			           else                   ns = ps;
+			flushing:  if (transComplete)     ns = lowPower;
+						  else                   ns = ps;
+			default:                          ns = 3'bxxx
+			
 	
 	
-	// Combinational Logic
-	always_comb begin
-		
-	end
 	
+	  endcase
+	
+
 	// Sequential Logic
 	always_ff @(posedge clk) begin
 		if (reset) begin
-			
+			ps <= lowPower;	
 		end else begin
 			ps <= ns;
 		end
 	end
 	
 endmodule
+ 
+ 
