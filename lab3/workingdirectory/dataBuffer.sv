843df8f95b5760edba4db8fee28516c69ebe0ff8
andriqueliu <andriqueliu@gmail.com>
2017-02-01 18:08:22 -0800

    Still fleshing out. Asked TA about states.

--- lab3/workingdirectory/dataBuffer.sv
+++ lab3/workingdirectory/dataBuffer.sv
@@ -8,8 +8,7 @@ dataBuffer controls the data level of the data buffer.
 
 Note: Assume we are using 25 MHz clock (whichClock = 0)
 
-Note: Since Quartus does not allow floats, int bufferAmount goes up to 20 in
-increments of 1 instead of up to 10 in increments of 0.5.
+
 */
 module dataBuffer (clk, reset, level80, level90, level100);
 	input  logic clk, reset;
@@ -26,15 +25,15 @@ module dataBuffer (clk, reset, level80, level90, level100);
 	
 	// Combinational Logic
 	always_comb begin
-		if (bufferAmount == 16) begin
+		if (bufferAmount == 8) begin
 			level80 = 1;
 			level90 = 0;
 			level100 = 0;
-		end else if (bufferAmount == 18) begin
+		end else if (bufferAmount == 9) begin
 			level80 = 0;
 			level90 = 1;
 			level100 = 0;
-		end else if (bufferAmount == 20) begin
+		end else if (bufferAmount == 10) begin
 			level80 = 0;
 			level90 = 0;
 			level100 = 1;
@@ -45,12 +44,13 @@ module dataBuffer (clk, reset, level80, level90, level100);
 		end
 	end
 	
+	// Seq Log needs testing
 	// Sequential Logic
 	always_ff @(posedge clk) begin
 		if (reset) begin
 			count <= 0;
 			bufferAmount <= 0;
-		end else if (count == (25000000 - 1)) begin // Probably want a smaller value for testing in MS
+		end else if ((count == (25000000 - 1)) && (buffer <= 9)) begin
 			count <= 0;
 			bufferAmount <= bufferAmount + 1;
 		end else begin
--- lab3/workingdirectory/scanner.sv
+++ lab3/workingdirectory/scanner.sv
@@ -1,19 +1,25 @@
-// EE 371 Winter 2017
-// Authors: Emraj Sidhu, Andrique Liu, Nikhil Grover 
-// Finite State Machine for the scanner
 
-// The scanning state in the FSM may need some work. Directions for that are a bit confusing
-// We may have too many or the completely wrong inputs. We need to talk about the databuffer module and
-// how the rest of the system will be designed.
+
+
 
 // Ideally, there should be one transfer signal for the whole system. But I used two for now out of convenience 
 // for each of the two buffers
 
+/*
+EE 371 Winter 2017
+Lab 3 Project
+Authors: Emraj Sidhu, Andrique Liu, Nikhil Grover
+
+Finite State Machine for the Scanner 
+
+
+
+*/
 module scanner (clk, reset, standBySig, startScanning, transferCmd, secondTransferCmd, transComplete, flush, stateOut );
 	input  logic clk, reset;
-	input  logic standBySign, startScanning, transferCmd, secondTransferCmd, transComplete, flush;
+	input  logic standBySign, startScanning, transfer, secondTransferCmd, transComplete, flush;
 	
-	output logic stateOut; //is output going to hex????
+	output logic [:0] status;
 	
 	
 	
@@ -21,12 +27,12 @@ module scanner (clk, reset, standBySig, startScanning, transferCmd, secondTransf
 	dataBuffer dataBuff (.clk, .reset, .level80(), .level90(), .level100());
 	
 	// State Variables
-	enum { LOWPOWER, STANDBY, SCANNING, IDLE, TRANSFER, FLUSH } ps, ns;
+	enum { LOWPOWER, STANDBY, ACTIVE, IDLE, TRANSFER, FLUSH } ps, ns;
 	
 	// Combinational Logic
 	always_comb begin
 		case (ps) 
-			lowPower:
+			LOWPOWER:
 				if (standBySig)
 					ns = standby;
 				else
@@ -36,17 +42,32 @@ module scanner (clk, reset, standBySig, startScanning, transferCmd, secondTransf
 					ns = scanning;
 				else
 					ns = ps;
-			scanning:  if (transferCmd)       ns = transfer; 
-						  else if (!transferCmd) ns = idle;
-						  else                   ns = ps;
-			idle:      if (secondTransferCmd) ns = transfer;
-						  else if (flush)        ns = flushing;
-				        else                   ns = ps;
-			transfer:  if (transComplete)     ns = lowPower;
-			           else                   ns = ps;
-			flushing:  if (transComplete)     ns = lowPower;
-						  else                   ns = ps;
-			default:                          ns = 3'bxxx
+			scanning:
+				if (transferCmd)
+					ns = transfer; 
+				else if (!transferCmd)
+					ns = idle;
+				else
+					ns = ps;
+			idle:
+				if (secondTransferCmd)
+					ns = transfer;
+				else if (flush)
+					ns = flushing;
+				else
+					ns = ps;
+			transfer:
+				if (transComplete)
+					ns = lowPower;
+			   else
+					ns = ps;
+			flushing:
+				if (transComplete)
+					ns = lowPower;
+				else
+					ns = ps;
+			default:
+				
 	  endcase
 
 	// Assign output
