119d462184321557d650627654af96376796d514
andriqueliu <andriqueliu@gmail.com>
2017-02-18 19:22:13 -0800

    Add files

---
+++ lab4/verilog/dataCollect.sv
@@ -0,0 +1,172 @@
+/*
+memory functions as a static RAM, with 2K (2048) x 16-bit memory locations.
+From an external view, memory functionally has 1K (1024) x 32-bit memory locations.
+This means that addresses entered into this module are assumed to range from 0 to 1023.
+
+memory supports RW operations, executing these operations using a 32-bit I/O port.
+
+To operate memory, clear chip_select to select the device and enable R/W operations. 
+Reading: Clear output enable, enter address into MAR, set RW
+Writing: Set output enable, enter address and write data in MDR, and clear RW
+
+Note: Signals in memory propagate after delay (example: when reading, it will take
+a propagation delay after entering an address to display memory's data).
+
+Note: Reset operation is NOT supported in memory module, as it is too costly
+to wipe the memory module.
+*/
+module dataCollect (clk, reset, data, address, out_en, active, RW);
+   input  logic clk, reset;     // Clock, Reset signals
+	inout  [7:0] data;           // Bidirectional 32-bit I/O port
+	// 11-bit address input- see Note.
+	// Note: From an external view, system interprets memory as having 32-bits/word per
+	// location. This way, the memory is functionally word-addressable, with
+	// 1024 functional 32-bit memory blocks.
+	// Note: Within the memory, each location has 16-bits, for a total of 2048 16-bit
+	// memory blocks.
+	input  integer address;
+	// Output enable: 1 to write to data, 0 to read from data
+   input  logic out_en;
+	// Chip select: 1 to disable R/W, 0 to enable R/W
+	input  logic active;
+   // Read/Write: 1 to read, 0 to write to memory
+	// Assume write takes place on posedge of RW (if RW is strobed for one cycle)
+	// Note: If write is held low for more than one cycle, data is simply written until
+	// one cycle after RW goes high.
+	input  logic RW;
+	
+	// Memory Data Registers: higher bits and lower bits
+	logic  [7:0] MDR;
+	
+	// 16-bit values with a depth of 
+   logic  [7:0] stored_memory [750];	
+	
+	// Combinational Logic
+	// Output enable behavior:
+	// If 1, IO port gets high Z (Write)
+	// If 0, IO port gets data_out (Read)
+	assign data = (!out_en && active && RW) ? MDR : 8'bZ;
+	
+	// Sequential Logic
+	always_ff @(posedge clk) begin
+		if (RW && !out_en && active) begin            // Read from memory
+			MDR <= stored_memory[address];
+		end else if (!RW && out_en && active) begin   // Write to memory
+			stored_memory[address] <= data;
+		end else begin                                // Default: Unknown values
+			MDR <= 8'bX;
+		end
+	end
+	
+endmodule
+
+// Tester Module
+module dataCollect_testbench();
+	logic clk, reset;     // Clock, Reset signals
+	wire [7:0] data;          // Bidirectional 32-bit I/O port
+	// 11-bit address input- see Note.
+	// Note: From an external view, system interprets memory as having 32-bits/word per
+	// location. This way, the memory is functionally word-addressable, with
+	// 1024 functional 32-bit memory blocks.
+	// Note: Within the memory, each location has 16-bits, for a total of 2048 16-bit
+	// memory blocks.
+	integer address;
+	// Output enable: 1 to write to data, 0 to read from data
+   logic out_en;
+	// Chip select: 1 to disable R/W, 0 to enable R/W
+	logic active;
+   // Read/Write: 1 to read, 0 to write to memory
+	// Assume write takes place on posedge of RW (if RW is strobed for one cycle)
+	// Note: If write is held low for more than one cycle, data is simply written until
+	// one cycle after RW goes high.
+	logic RW;
+	
+	logic [7:0] data1;
+	
+	dataCollect dut (clk, reset, data, address, out_en, active, RW);
+	
+	// Note: when ACCESSING memory, output enable logic is inverted
+	// (depending which way you define it)
+	// ...
+	// Need to add chip select, all those other signals...
+//	assign data = (out_en) ? data1 : 16'bz;
+	assign data[7:0] = (out_en) ? data1 : 8'bZ;
+	
+	// Set up the clock.
+	parameter CLOCK_PERIOD=100;
+	initial begin
+		clk <= 0;
+		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	end
+	
+	// Set up the inputs to the design. Each line is a clock cycle.
+	// Test... set direction to pass data in
+	// Then clear direction to read data
+	initial begin
+								  @(posedge clk);
+	active <= 1; 	        @(posedge clk);
+								  @(posedge clk);
+	RW <= 1;               @(posedge clk);
+								  @(posedge clk);
+	out_en <= 1; address <= 0; data1 <= 3; @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+	RW <= 0;               @(posedge clk);
+	RW <= 1;               @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+	address <= 2; data1 <= 5; @(posedge clk);
+								  @(posedge clk);
+	RW <= 0;               @(posedge clk);
+	RW <= 1;               @(posedge clk);
+	address <= 5; data1 <= 2; @(posedge clk);
+								  @(posedge clk);
+	RW <= 0;               @(posedge clk);
+	RW <= 1;               @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+	out_en <= 0; address <= 5; @(posedge clk);
+								  @(posedge clk);
+	address <= 2;     @(posedge clk);
+	address <= 0;  @(posedge clk);
+
+
+
+////	out_en <= 1; address <= 11'b00; data1 <= {16{1'b1}}; data2 <= {16{1'b1}}; @(posedge clk);
+//	out_en <= 1; address <= 11'b00; data1 <= {16{1'b1}}; data2 <= {16{1'b0}}; @(posedge clk);
+//								  @(posedge clk);
+//								  @(posedge clk);
+//	RW <= 0;               @(posedge clk);
+//	RW <= 1;               @(posedge clk);
+//								  @(posedge clk);
+//								  @(posedge clk);
+//	address <= 11'b01; data1 <= {16{1'b0}}; data2 <= {16{1'b0}}; @(posedge clk);
+//								  @(posedge clk);
+//	RW <= 0;               @(posedge clk);
+//	RW <= 1;               @(posedge clk);
+//	address <= 11'b01000; data1 <= 16'b0000111100001111; data2 <= 16'b0000111100001111; @(posedge clk);
+//								  @(posedge clk);
+//	RW <= 0;               @(posedge clk);
+//	RW <= 1;               @(posedge clk);
+//								  @(posedge clk);
+//								  @(posedge clk);
+//	out_en <= 0; address <= 11'b010; @(posedge clk);
+//								  @(posedge clk);
+//	address <= 11'b01;     @(posedge clk);
+//	address <= 11'b01000;  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+//	out_en <= 1; address <= 4'b0111; data1 <= 4'b0000; @(posedge clk);
+//	RW <= 0;               @(posedge clk);
+//	RW <= 1;               @(posedge clk);
+//	out_en <= 0;           @(posedge clk);
+//	address <= 4'b0110;    @(posedge clk);
+//								  @(posedge clk);
+//	address <= 4'b0000;    @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+	$stop; // End the simulation.
+	end
+endmodule
---
+++ lab4/verilog/dataCollectTop.sv
@@ -0,0 +1,219 @@
+// !!!
+// This module's processes can probably be written as a state machine.
+// States determine variables such as out en, etc., whereas sequential logic
+// can deal with latches such as address <= address + 1, etc.
+
+/*
+memoryTester is used to test the SRAM module; memoryTester first writes data into
+the memory, and then reads them out, displaying them on the LEDs.
+
+
+*/
+module dataCollectTop (clk, reset, data, startWrite, startRead, clkLight, transferBit, clkOut, lights,
+                       stateHEX, pctgHEX);
+   input  logic  clk, reset;            // Clock, Reset signals
+	inout  [7:0] data;          // Bidirectional 32-bit I/O port
+	// 11-bit address input- see Note.
+	// Note: From an external view, system interprets memory as having 32-bits/word per
+	// location. This way, the memory is functionally word-addressable, with
+	// 1024 functional 32-bit memory blocks.
+	// Note: Within the memory, each location has 16-bits, for a total of 2048 16-bit
+	// memory blocks.
+	
+	// Output enable: 1 to write to data, 0 to read from data
+	
+	// Active:
+	
+   // Read/Write: 1 to read, 0 to write to memory
+	// Assume write takes place on posedge of RW (if RW is strobed for one cycle)
+	// Note: If write is held low for more than one cycle, data is simply written until
+	// one cycle after RW goes high.
+	// Board peripherals
+	input  logic startWrite, startRead;
+	output logic clkLight;
+	
+	
+	output logic transferBit;
+	output logic clkOut;
+	
+	output logic [7:0] lights;
+	
+	
+	output logic [6:0] stateHEX, pctgHEX;
+	
+	assign lights = data;
+	
+	// Assign signals
+	assign clkLight = clk;
+	
+	// Status signals: Read and Write 
+	logic  writeReady, readReady;
+	
+	// Memory Address Registers: Adjacent addresses
+	integer address;
+	// Memory Data Registers: Combined
+	logic  [7:0] MDR;
+	// Output enable, chip select, RW
+	logic  out_en, active, RW;
+	
+	
+	// 
+	integer i;
+	
+	// Delay iterator; 
+	integer delayItr;
+	
+	
+	
+	
+	// 
+	dataCollect collector (.clk, .reset, .data, .address, .out_en, .active, .RW);
+	
+	
+	// 
+	pctgDisplay pHEX (.HEX5(pctgHEX), .address);
+	
+	// 
+	stateDisplay sHEX (.clk, .reset, .startScanning(startWrite),
+	                   .startTransfer(startRead), .address, .HEX(stateHEX));
+	
+	// Initialize variables
+	initial begin
+		address = 0;;
+		MDR = {7{1'b0}};
+		writeReady = 1;
+		readReady  = 1;
+		
+		
+		i = 0;
+		
+		delayItr = 0;
+	end
+	
+	// Combinational Logic
+	// Output enable behavior:
+	// If 1, IO port gets high Z (Write)
+	// If 0, IO port gets data_out (Read) ??? Not sure if these are inverted, check later
+	assign data[7:0] = !(!out_en && active && RW) ? MDR : 7'bZ; // !!! Negation
+	
+//	// 
+//	assign clkOut = startRead ? clk : 1'bX;
+//	// 
+//	assign transferBit = startRead ? data[i] : 1'bX;
+	
+	// ??? What if it shouldn't be high X, what if it should be known? Like 0?
+	// 
+//	assign clkOut = startRead ? clk : 1'b0;
+//	// 
+//	assign transferBit = startRead ? data[i] : 1'b0;
+	
+	
+	
+	always_comb begin
+		if (startRead) begin
+			clkOut = clk;
+			transferBit = data[i];
+		end else begin
+			clkOut = 1'b0;
+			transferBit = 1'b0;
+		end
+	end
+	
+	
+	
+	// Sequential Logic
+	// Reset: reset variables to initial variables
+	// Else, if receive startWrite command and write is ready, begin
+	// scanning/writing into memory.
+	// Else, if receive startRead command and read is ready, begin
+	// sending out serial data (transferring scanned data)
+	always_ff @(posedge clk) begin
+		if (reset) begin
+			address <= 0;
+			MDR <= 0;
+			writeReady <= 1;
+			readReady  <= 1;
+			RW <= 0;
+			out_en <= 1;
+			active <= 1;
+			
+			i = 0;
+		end else if (startWrite && writeReady) begin
+			if (address < 749) begin
+//			if (address < 14) begin
+				if (delayItr < 14) begin
+					delayItr <= delayItr + 1;
+				end else if (delayItr == 14) begin
+					delayItr <= delayItr + 1;
+					MDR <= MDR + 1;
+					address <= address + 1;
+				end else if (delayItr == 15) begin
+					delayItr <= 0;
+				end
+			end else begin
+				writeReady <= 0;
+				RW <= 1;
+				out_en <= 0;
+				address <= 749;
+//				address <= 14;
+			end
+		end else if (startRead && readReady) begin
+			if (address > -1) begin
+				if (i < 6) begin
+					i <= i + 1;
+				end else if (i == 6) begin
+					i <= i + 1;
+					address <= address - 1;
+				end else if (i == 7) begin
+					i <= 0;
+				end
+			end else begin
+				readReady <= 0;
+				address <= -1;
+			end
+		end
+	end
+	
+endmodule
+
+// Tester Module
+module dataCollectTop_testbench();
+	logic  clk, reset;            // Clock, Reset signals
+	wire [7:0] data;              // Bidirectional 32-bit I/O port
+	logic startWrite, startRead;
+	logic clkLight;
+	logic transferBit;
+	logic clkOut;
+	logic [7:0] lights;
+	
+	dataCollectTop dut (clk, reset, data, startWrite, startRead, clkLight, transferBit, clkOut, lights);
+	
+	// Set up the clock.
+	parameter CLOCK_PERIOD=100;
+	initial begin
+		clk <= 0;
+		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	end
+	
+	// Set up the inputs to the design. Each line is a clock cycle.
+	// Test... set direction to pass data in
+	// Then clear direction to read data
+	initial begin
+								  @(posedge clk);
+	reset <= 1;            @(posedge clk);
+	reset <= 0;            @(posedge clk);
+	@(posedge clk);
+	@(posedge clk);
+	startWrite <= 1;
+						repeat (300) @(posedge clk);
+	startRead <= 1;
+						repeat (300) @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+								  @(posedge clk);
+	$stop; // End the simulation.
+	end
+endmodule
