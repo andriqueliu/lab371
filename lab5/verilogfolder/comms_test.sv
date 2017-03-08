fe2226a0c0c1cecd851f60bc9f0cbf0fd5299602
andriqueliu <andriqueliu@gmail.com>
2017-03-08 11:52:56 -0800

    Add video driver files

--- lab5/verilogfolder/comms_test.sv
+++ lab5/verilogfolder/comms_test.sv
@@ -24,22 +24,6 @@ module comms_test (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW, 
 			GPIO_0[i] = (out_en[i]) ? GPIO_0_in[i] : 1'bZ;
 		end
 	end
-	
-//	// Configure outputs
-//	// GPIOs 35, 34, 32, 25, 24 are outputs
-//	assign out_en[35] = 1'b1;
-//	assign out_en[34] = 1'b1;
-//	assign out_en[32] = 1'b1;
-//	assign out_en[25] = 1'b1;
-//	assign out_en[24] = 1'b1;
-//	
-//	// Configure inputs
-//	// GPIOs 33, 31, 29, 28, 26 are inputs
-//	assign out_en[33] = 1'b0;
-//	assign out_en[31] = 1'b0;
-//	assign out_en[29] = 1'b0;
-//	assign out_en[28] = 1'b0;
-//	assign out_en[26] = 1'b0;
 
 	// Configure outputs
 	assign out_en[0] = 1'b1; // Reset out
@@ -55,20 +39,17 @@ module comms_test (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW, 
 	// Configure outputs
 	assign out_en[7] = 1'b1; // Ready out
 	
-	
-	
+	// 
 	logic  reset;
-//	assign reset = ~KEY[3];
 	assign reset = (~KEY[3] || GPIO_0[4]);
-//	assign GPIO_0[0] = ~KEY[3];
 	assign GPIO_0_in[0] = ~KEY[3];
 	
+	// 
 	logic  enter, enter_out;
 	assign enter = ~KEY[2];
-	
 	uinput (.clk(CLOCK_50), .reset, .in(enter), .out(enter_out));
 	
-//	logic [2:0] local_reg;
+	// 
 	logic [6:0] local_reg;
 	
 	assign LEDR[6:0] = local_reg;
@@ -88,25 +69,14 @@ module comms_test (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW, 
 		
 	end
 	
-//	always_ff @(CLOCK_50) begin
-//		if (local_reg == 7'b0000001) begin
-//			LEDR[0] <= 1;
-//		end else if (local_reg == 7'b0000010) begin
-//			LEDR[1] <= 1;
-//		end else if (local_reg == 7'b0000100) begin
-//			LEDR[2] <= 1;
-//		end
-//	end
-	
+	// 
 	serial_out ser_out (.clk(CLOCK_50), .reset, .ready_in( ), .column(SW[6:0]),
 	                    .enter(enter_out), .clk_out(GPIO_0_in[1]), .bit_out(GPIO_0_in[2]));
 	
+	// 
 	serial_in ser_in (.clk(CLOCK_50), .reset, .clk_in(GPIO_0[5]), .bit_in(GPIO_0[6]),
 	                  .column_select(local_reg));
-	
-	// Assign reference clock for debugging and probing
-//	assign LEDR[9] = clk[whichClock];
-	
+		
 	// Turn off unused hex displays
 	assign HEX0[6:0] = ~(7'b0000000);
 	assign HEX1[6:0] = ~(7'b0000000);
